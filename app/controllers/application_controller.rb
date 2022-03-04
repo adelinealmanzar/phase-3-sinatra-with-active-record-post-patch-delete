1b377a8529e34ceeb74c6b81919949abdab59b7c
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-04 12:12:31 -0800

    commit tree 268f0512beb1f2fe1450a7ac4c03d5895ebe8ac1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +15,36 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get '/reviews' do
+    review = Review.all
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy #delete review in database
+    review.to_json #send response with deleted review
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score], #params is basically the user's/frontend's data
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
